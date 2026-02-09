bc9cbd1fe5919d33a7a0e49114cead596ccf002d
Basti <thielke.sebastian@gmail.com>
2026-02-09 22:00:20 +0100

    Email bestätigen für register

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
+
 use Illuminate\Http\RedirectResponse;
 
 class VerifyEmailController extends Controller
@@ -13,7 +13,7 @@ class VerifyEmailController extends Controller
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    public function __invoke(\Illuminate\Http\Request $request): RedirectResponse
     {
         if ($request->user() && $request->user()->hasVerifiedEmail()) {
             return redirect()->intended(RouteServiceProvider::HOME . '?verified=1');
